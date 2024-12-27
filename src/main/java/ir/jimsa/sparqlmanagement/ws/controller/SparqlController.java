8e1ef7905cf15ed212291ea8aeca33647b8bc645
Iman Salehi <cse.isalehi@gmail.com>
2024-12-27 11:37:05 +0330

    impl controller

--- src/main/java/ir/jimsa/sparqlmanagement/ws/controller/SparqlController.java
+++ src/main/java/ir/jimsa/sparqlmanagement/ws/controller/SparqlController.java
@@ -4,39 +4,89 @@ import ir.jimsa.sparqlmanagement.ws.model.dto.ResponseDto;
 import ir.jimsa.sparqlmanagement.ws.model.dto.SparqlDto;
 import ir.jimsa.sparqlmanagement.ws.service.SparqlService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/sparql", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class SparqlController {
 
     private final SparqlService sparqlService;
 
-    @GetMapping
-    public ResponseEntity<ResponseDto> readAllSparql() {
-        return null;
-    }
-
-    @GetMapping("/{public-id}")
-    public ResponseEntity<ResponseDto> readSparql(@PathVariable String publicId) {
-        return null;
-    }
-
     @PostMapping
     public ResponseEntity<ResponseDto> createSparql(@RequestBody SparqlDto sparqlDto) {
-        return null;
+        SparqlDto savedSparql = sparqlService.create(sparqlDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ResponseDto.builder()
+                                .action(true)
+                                .timestamp(LocalDateTime.now())
+                                .result(savedSparql)
+                                .build()
+                );
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto> readAllSparql(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        List<SparqlDto> sparqlDtos = sparqlService.read(page, size);
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                                .action(true)
+                                .timestamp(LocalDateTime.now())
+                                .result(sparqlDtos)
+                                .build()
+                );
+    }
+
+    @GetMapping("/{public_id}")
+    public ResponseEntity<ResponseDto> readSparql(@PathVariable("public_id") String publicId) {
+        SparqlDto sparqlDto = sparqlService.read(publicId);
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                                .action(true)
+                                .timestamp(LocalDateTime.now())
+                                .result(sparqlDto)
+                                .build()
+                );
     }
 
     @PutMapping("/{public_id}")
-    public ResponseEntity<ResponseDto> updateSparql(@PathVariable String publicId, @RequestBody SparqlDto sparqlDto) {
-        return null;
+    public ResponseEntity<ResponseDto> updateSparql(@PathVariable("public_id") String publicId, @RequestBody SparqlDto sparqlDto) {
+        SparqlDto updatedSparqlDto = sparqlService.update(publicId, sparqlDto);
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                                .action(true)
+                                .timestamp(LocalDateTime.now())
+                                .result(updatedSparqlDto)
+                                .build()
+                );
     }
 
-    @DeleteMapping("/{public-id}")
-    public ResponseEntity<ResponseDto> deleteSparql(@PathVariable String publicId) {
-        return null;
+    @DeleteMapping("/{public_id}")
+    public ResponseEntity<ResponseDto> deleteSparql(@PathVariable("public_id") String publicId) {
+        boolean result = sparqlService.delete(publicId);
+        return ResponseEntity
+                .ok(
+                        ResponseDto.builder()
+                                .action(true)
+                                .timestamp(LocalDateTime.now())
+                                .result(result)
+                                .build()
+                );
     }
 }
--- src/main/java/ir/jimsa/sparqlmanagement/ws/service/SparqlService.java
+++ src/main/java/ir/jimsa/sparqlmanagement/ws/service/SparqlService.java
@@ -1,4 +1,17 @@
 package ir.jimsa.sparqlmanagement.ws.service;
 
+import ir.jimsa.sparqlmanagement.ws.model.dto.SparqlDto;
+
+import java.util.List;
+
 public interface SparqlService {
+    SparqlDto create(SparqlDto sparqlDto);
+
+    List<SparqlDto> read(int page, int size);
+
+    SparqlDto read(String publicId);
+
+    SparqlDto update(String publicId, SparqlDto sparqlDto);
+
+    boolean delete(String publicId);
 }
