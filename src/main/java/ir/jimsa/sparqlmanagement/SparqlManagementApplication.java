2e4f7d9d9ff67e08a31ebffd135ac0b06dc221ea
Iman Salehi <cse.isalehi@gmail.com>
2024-12-27 10:43:18 +0330

    init project

---
+++ src/main/java/ir/jimsa/sparqlmanagement/SparqlManagementApplication.java
@@ -0,0 +1,13 @@
+package ir.jimsa.sparqlmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparqlManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparqlManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/jimsa/sparqlmanagement/SparqlManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.jimsa.sparqlmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparqlManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
