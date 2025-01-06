615e4f6ef3142bac19eaa490d102ceeb68392cbb
Iman Salehi <cse.isalehi@gmail.com>
2025-01-06 09:32:59 +0330

    fixbug: change id's type

--- src/main/java/ir/jimsa/sparqlmanagement/ws/repository/SparqlRepository.java
+++ src/main/java/ir/jimsa/sparqlmanagement/ws/repository/SparqlRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface SparqlRepository extends JpaRepository<SparqlEntity, String> {
+public interface SparqlRepository extends JpaRepository<SparqlEntity, Long> {
     Optional<SparqlEntity> findByPublicId(String publicId);
 }
